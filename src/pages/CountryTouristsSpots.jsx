048e2dbb885801785b5d317f74a8adc19120f471
GearHead87 <hosanulislam87@gmail.com>
2024-04-28 22:43:11 +0600

    margin added in country tourists spots page

--- src/pages/CountryTouristsSpots.jsx
+++ src/pages/CountryTouristsSpots.jsx
@@ -10,7 +10,7 @@ const CountryTouristsSpots = () => {
             <Helmet>
                 <title>{params.country_Name} - Tourists Spots</title>
             </Helmet>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-5">
 
                 {
                     touristSpots.map(spot => (
